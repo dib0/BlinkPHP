d16ccaee3fe5871857a69fc33099291bd81d3599
Bas van den Berg <bas@dib0.nl>
2016-12-06 10:53:58 +0100

    Added arm/disarm functions

--- api.php
+++ api.php
@@ -12,8 +12,13 @@ function httpPost($url, $data)
                                         'Content-Length: '.strlen($data)
                                         ));
     curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLINFO_HEADER_OUT, true);
 
     $response = curl_exec($curl);
+
+    $info = curl_getinfo($curl);
+    echo $info['request_header'];
+
     curl_close($curl);
 
     return $response;
@@ -21,6 +26,8 @@ function httpPost($url, $data)
 
 function httpPostToken($url, $data, $token)
 {
+    global $devtoken;
+
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
@@ -31,8 +38,13 @@ function httpPostToken($url, $data, $token)
                                         'Content-Length: '.strlen($data)
                                         ));
     curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLINFO_HEADER_OUT, true);
 
     $response = curl_exec($curl);
+
+    $info = curl_getinfo($curl);
+    echo $info['request_header'];
+
     curl_close($curl);
 
     return $response;
@@ -40,37 +52,65 @@ function httpPostToken($url, $data, $token)
 
 function httpGet($url, $token)
 {
+    global $devtoken;
+
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_HTTPHEADER, array(
                                         'Content-Type: application/json',
                                         'TOKEN_AUTH: '.$token
                                         ));
+    curl_setopt($curl, CURLINFO_HEADER_OUT, true);
     $response = curl_exec($curl);
+
+    $info = curl_getinfo($curl);
+    echo $info['request_header'];
+
     curl_close($curl);
 
     return $response;
 }
 
 function login() {
-    global $password, $clientId, $username, $serveruri, $authtoken;
+    global $password, $clientId, $username, $serveruri, $authtoken, $networkId;
 
-    $data = array(
-	"password" => $password,
-	"client_specifier" => $clientId,
-	"email" => $username
-    );
+    if ($authtoken === "")
+    {
+	echo "Login<br />";
+	$data = array(
+	    "password" => $password,
+	    "client_specifier" => $clientId,
+	    "email" => $username
+	);
 
-    $result=httpPost($serveruri."/login", json_encode($data));
-    $result=json_decode($result);
+	$result=httpPost($serveruri."/login", json_encode($data));
+	$result=json_decode($result, true);
 
-    $authtoken=$result->{'authtoken'}->{'authtoken'};
+	$authtoken=$result['authtoken']['authtoken'];
+	$networks=array_keys($result['networks']);
+	$networkId=$networks[0];
+    }
 }
 
 function getNetworks() {
     global $serveruri, $authtoken;
 
-    $result=httpGet($serveruri."/networks", $authtoken);
+    $result=httpGet($serveruri."/account/clients", $authtoken);
+    echo $result;
+}
+
+function arm() {
+    global $serveruri, $authtoken, $networkId;
+
+    $result=httpPostToken($serveruri."/network/".$networkId."/arm", "", $authtoken);
+    echo $result;
+}
+
+function disarm() {
+    global $serveruri, $authtoken, $networkId;
+
+    $result=httpPostToken($serveruri."/network/".$networkId."/disarm", "", $authtoken);
     echo $result;
 }
 ?>
